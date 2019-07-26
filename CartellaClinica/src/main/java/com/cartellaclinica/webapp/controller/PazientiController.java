a7e12744a3b3c266229ade55c37f5bb559237f99
UFFICIOTRE <UFFICIOTRE@UFFICIOTRE-PC>
2019-07-26 12:35:30 +0200

    start

--- CartellaClinica/src/main/java/com/cartellaclinica/webapp/controller/PazientiController.java
+++ CartellaClinica/src/main/java/com/cartellaclinica/webapp/controller/PazientiController.java
@@ -1,10 +1,16 @@
 package com.cartellaclinica.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cartellaclinica.webapp.service.AnagraficaService;
 
 @Controller
 @RequestMapping("/pazienti")
 public class PazientiController {
+
+	@Autowired
+	private AnagraficaService anagraficaService;
 
 }
